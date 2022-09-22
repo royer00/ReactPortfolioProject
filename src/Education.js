423a79093ecccb1eddf05235c06ab585b9925094
Kevin Royer <royer00@gmail.com>
2022-09-22 16:39:18 +0000

    add education information and css

--- src/Education.js
+++ src/Education.js
@@ -1,19 +1,24 @@
-import React from 'react';
-
-
-
-
+import React from "react";
+import "./education.css";
 
 const Education = () => {
-    
-    return (
-        <>
-        Education
-        </>
-    )
-}
-
-
-
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">Centriq Training</span>
+            <span className="university-degree">
+              Full-stack Web Development
+            </span>
+          </div>
+          <div>
+            <span className="year-passedout">2017-2021</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Education;
